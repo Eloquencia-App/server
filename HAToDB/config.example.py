9f338f436b1106416f9a9f7461652ea94428e9e4
Ethan <41381482+ethandudu@users.noreply.github.com>
2024-08-22 18:47:45 +0200

    example config file
    sh: line 1: q: command not found

---
+++ HAToDB/config.example.py
@@ -0,0 +1,17 @@
+# Database configuration
+DATABASE_HOST = ''
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_NAME = ''
+
+# HelloAssos configuration
+helloasso_client_id = ''
+helloasso_client_secret = ''
+helloasso_organization_slug = ''
+helloasso_form_slug = ''
+
+# SMTP configuration
+SMTP_SERVER = ''
+SMTP_PORT = 587
+SMTP_USER = ''
+SMTP_PASSWORD = ''
